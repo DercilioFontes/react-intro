e366399e3755893773931a513245874508178a57
DercilioFontes <dercilioafontes@gmail.com>
2018-09-12 19:10:49 -0700

    Added Greetings componet.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Greetings = (props) => 
+  <div>Hey you! {props.firstName} {props.lastName}!</div>;
+
 const App = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
     </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+    <br />
+    <Greetings firstName="Dercilio" lastName="Fontes"/>
   </div>
 );
 
